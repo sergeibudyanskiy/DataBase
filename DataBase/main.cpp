3912ea77795daa722cbba5e343198a5f681a08df
sergeibudyanskiy <sergeibudyanskiy@gmail.com>
2021-09-25 14:22:25 +0300

    Initial Commit

---
+++ DataBase/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataBase
+//
+//  Created by Sergei Budyanskiy on 25.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
